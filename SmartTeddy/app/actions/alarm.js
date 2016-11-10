13b8710813be5f7b6c7e861f0a5293983859e1ef
niggor <zotov.bmstu@gmail.com>
2016-11-10 03:16:00 +0300

    fix warnings and errors

--- SmartTeddy/app/actions/alarm.js
+++ SmartTeddy/app/actions/alarm.js
@@ -50,7 +50,7 @@ export function getAlarmTime () {
     let instance = Bluetooth.getInstance();
     return function (dispatch, getState) {
         return instance.getAlarmTime()
-            .then((res) => { this.parseTime(res,dispatch,getState ) })
+            .then((res) => { parseTime(res,dispatch,getState ) })
             .catch((error) => {
                 console.log('getAlarmTime error');
                 console.log(error)
@@ -60,24 +60,25 @@ export function getAlarmTime () {
 
 function parseTime (res, dispatch, getState) {
     let d = res.charCodeAt(2);
-    let time;
+    let time = new Date();
     time.setHours(res.charCodeAt(0), res.charCodeAt(1));
 
     let active = res.charCodeAt(3);
     let days = [];
     for (let i = 0; i < 7; ++i) {
-        days[i] = (d >> i) & 0x01
+        days[i] = ((d >> i) & 0x01) ? true : false;
     }
 
-    let alarm_active = ((active >> 3) & 0x01);
-    let alarm_lightActive = ((active >> 0) & 0x01);
-    let alarm_vibroActive = ((active >> 1) & 0x01);
-    let alarm_soundActive = ((active >> 2) & 0x01);
+    let alarm_active = ((active >> 3) & 0x01) ? true : false;
+    let alarm_lightActive = ((active >> 0) & 0x01) ? true : false;
+    let alarm_vibroActive = ((active >> 1) & 0x01) ? true : false;
+    let alarm_soundActive = ((active >> 2) & 0x01) ? true : false;
     let state = getState();
-    if (state.alarm.active !== alarm_active) dispatch(toggleAlarmActive());
-    if (state.alarm.lightActive !== alarm_lightActive) dispatch(toggleAlarmLight());
-    if (state.alarm.vibroActive !== alarm_vibroActive) dispatch(toggleAlarmVibro());
-    if (state.alarm.soundActive !== alarm_soundActive) dispatch(toggleAlarmSound());
+    console.log('state.alarm', state.alarm);
+    if (state.alarm.active == alarm_active) dispatch(toggleAlarmActive());
+    if (state.alarm.lightActive == alarm_lightActive) dispatch(toggleAlarmLight());
+    if (state.alarm.vibroActive == alarm_vibroActive) dispatch(toggleAlarmVibro());
+    if (state.alarm.soundActive == alarm_soundActive) dispatch(toggleAlarmSound());
     dispatch(setAlarmTime(time));
     //console.log('Reducer!!!!!!!');
     //console.log(days);
@@ -95,7 +96,7 @@ export function setAlarm () {
             soundActive: state.alarm.isSoundActive
         };
         return instance.setAlarm(state.alarm.alarmTime, state.alarm.alarmDays, activate)
-            .then((res) => { this.parseTime(res,dispatch,getState ) })
+            .then((res) => { parseTime(res,dispatch,getState ) })
             .catch((error) => {
                 console.log('setAlarm error');
                 console.log(error)
