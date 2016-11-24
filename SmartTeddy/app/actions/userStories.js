d3b20a5e1a6e1d067569491fe754f96ac1b50eec
anna <annjellyiu5@gmail.com>
2016-11-24 21:15:40 +0300

    add file

---
+++ SmartTeddy/app/actions/userStories.js
@@ -0,0 +1,37 @@
+import * as types from './actionTypes';
+
+function requestStories():Action {
+    return {
+        type: types.REQUEST_USER_STORIES
+    }
+}
+function receiveStories(json):Action {
+    let arr = json.body.stories;
+    let storiesArray=[];
+    arr.forEach(function(item,i,arr){storiesArray[item.id] = item});
+    return {
+        type: types.RECEIVE_USER_STORIES,
+        stories: storiesArray,
+        receivedAt: Date.now()
+    }
+}
+
+export function fetchStories() {
+
+    return function (dispatch,getState) {
+        dispatch(requestStories());
+        let url = 'https://hardteddy.ru/api/user/mystories';
+        let state = getState();
+        return fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': state.user.token
+            }
+        }).then((response) => response.json())
+            .then(json => dispatch(receiveStories(json))
+        ).catch((error) => {
+                console.log('fetch user stories error:');
+                console.log(error)
+            });
+    }
+}
